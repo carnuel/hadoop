80a2526c83f1ac43c600a4e5e1656b2184d66115
DBNet System Account <dbnet@ssds111.dbnet.syssoft.uni-trier.de>
2015-11-17 13:02:34 +0100

    if statement in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-cp /mnt/bootstrap.sh /etc/bootstrap.sh
-chown root:root /etc/bootstrap.sh && chmod 700 /etc/bootstrap.sh
+if [[ $1 == "-bash" || $1 == "-d" ]]; then
+        cp /mnt/bootstrap.sh /etc/bootstrap.sh
+	chown root:root /etc/bootstrap.sh && chmod 700 /etc/bootstrap.sh
 
-cp /mnt/copy.sh /etc/copy.sh
-chown root:root /etc/copy.sh && chmod 700 /etc/copy.sh
+	cp /mnt/copy.sh /etc/copy.sh
+	chown root:root /etc/copy.sh && chmod 700 /etc/copy.sh
 
-/bin/bash /etc/copy.sh
-/bin/bash /etc/bootstrap.sh $1
+	/bin/bash /etc/copy.sh
+	/bin/bash /etc/bootstrap.sh $1
+fi
